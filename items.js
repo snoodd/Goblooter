2d6a2a6b69f1a5718c7ba3a81cfafce4112769b6
snoodd <jeffmstone@gmail.com>
2024-05-02 07:05:28 -0400

    itemsDatastructure

--- items.js
+++ items.js
@@ -12,9 +12,9 @@ function createItem(x, y, value) {
         y: y,
         size: cellSize / 2,
         color: `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`, // Random color,
-        name: iteminfo[0],
-        value: value * iteminfo[1],
-        type: iteminfo[2],
+        name: iteminfo[0] + iteminfo[1],
+        value: value * iteminfo[2],
+        type: iteminfo[1],=8
         gridSizeX: iteminfo[3],
         gridSizeY: iteminfo[4],
         isDragging: false,
@@ -22,6 +22,7 @@ function createItem(x, y, value) {
         dragOffsetX: 0,
         dragOffsetY: 0
     };
+console.log(createItem);
 }
 
 // Function to check if an item is being dropped into the bag
@@ -55,125 +56,239 @@ function dropItemInBag(item) {
     }
 }
 
-class ItemAdjective {
+class ItemCategory {
     constructor(pWord, pValue) {
         this.word = pWord;
         this.value = pValue;
     }
 }
 
-function getBelievableItemName() {
+class Itemtype {
+    constructor(type, subType) {
+        this.type = type;
+        this.subType = subType;
+        this.value = pValue;
+    }
+}
+
+
+function getItemType() {
+    let itemCategory = "";
+    let itemType = "";
+    const things = ["Weapon", "Armor", "Accessory"];
+    const thing = getRandomElement(things);
+    const weaponTypes = ["Sword","Axe","Dagger","Polearm","Staff"];
+    const armorTypes = ["Head","Shoulder","Chest","Waist","Wrist","Hands","Feet","Back"];
+    const accessoryTypes = ["Neck", "Ring", "Trinket"];
+    const head_armor = ["Helm", "Crown", "Skullcap", "Circlet", "Visor"];
+    const shoulder_armor = ["Spaulders", "Mantle", "Pauldrons", "Epaulets", "Shoulderplates"];
+    const chest_armor = ["Breastplate", "Plate Mail", "Chainmail", "Robes", "Hauberk"];
+    const waist_armor = ["Belt", "Sash", "Girdle", "Waistband", "Cord"];
+    const wrist_armor = ["Bracers", "Cuffs", "Bands", "Vambraces", "Armguards"];
+    const hands_armor = ["Gauntlets", "Gloves", "Mitts", "Handguards", "Grips"];
+    const feet_armor = ["Boots", "Greaves", "Sabatons", "Shoes", "Sandals"];
+    const back_armor = ["Cloak", "Cape", "Mantle", "Shawl", "Wrap"];
+    const ring_types = ["Band", "Loop", "Seal", "Circle", "Signet"];
+    const necklace_types = ["Pendant", "Choker", "Chain", "Collar", "Torc"];
+    const trinket_types = ["Charm", "Bauble", "Token", "Ornament", "Relic"];
+
+/*function assignItemShape(size){
+ pItemsize = size;
+switch (pItemsize) {                
+        case '1x1':
+         shapeAssigned = [(0,0)];
+        break;
+        case '1x2':
+         shapeAssigned = [(0,0),(0,1)];
+        break;
+        case '1x3':
+         shapeAssigned = [(0,0),(0,1),(0,2)];
+        break;
+        case '1x4':
+         shapeAssigned = [(0,0),(0,1),(0,2),(0,3)];
+        break;
+default:
+    console.log(`Error`);
+}
+}*/
+
+const swordTypes = ["Claymore",
+                    "Rapier",
+                    "Katana",
+                    "Cutlass",
+                    "Longsword",
+                    "Scimitar",
+                    "Sabre",
+                    "Bastard sword",
+                    "Falchion",
+                    "Gladius"]
+class itemShape {
+    constructor(pSize,pVal) {
+        this.size = pSize;
+        this.val = pVal;
+    }
+}
+
+function getItemShape(){
+    class ItemTypeInfo {
+    constructor(pType, pShape) {
+        this.type = pType;
+        this.shape = pShape;
+    }
+}
+}
+
+
+        new ItemTypeInfo (getItemType(),      
+
+                        
+    const axeTypes  =   [
+        new ItemTypeInfo ("Wood Axe",      [(0,0),(1,0),(0,1),(1,1),(0,2),(1,2),(0,3),(1,3)]),
+        new ItemTypeInfo ("Tomahawk",      [(0,0),(0,1),(0,2)]),        
+        new ItemTypeInfo ("Labrys",        [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Bearded axe",   [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Throwing axe",  [(0,0),(0,1)]),
+        new ItemTypeInfo ("Bardiche",      [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Double Axe",    [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Hatchet",       [(0,0),(0,1),(0,2),(0,3)]),
+        new ItemTypeInfo ("Cleaver",       [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Broad Axe",     [(0,0),(0,1),(0,2)]),
+        new ItemTypeInfo ("Battle-axe",    [(0,0),(0,1),(0,2)])
+                        ];
+
+                        
+    const daggerTypes = ["Kris",
+                        "Dirk",
+                        "Main-gauche",
+                        "Stiletto",
+                        "Rondel",
+                        "Baselard"];
+                        
+    const polearmTypes = ["Glaive",
+                        "Halberd",
+                        "Pike",
+                        "Partisan",
+                        "Naginata",
+                        "Voulge",
+                        "Bec de corbin",
+                        "War scythe",
+                        "Ranseur"];
+                        
+    const staffTypes = ["Bo staff",
+                        "Quarterstaff",
+                        "Jo staff",
+                        "Shillelagh",
+                        "Yari",
+                        "Gunbai",
+                        "Sansetsukon"];
+
+
+
+    switch (thing) {                
+        case 'Weapon':
+         itemCategory = getRandomElement(weaponTypes);
+        break;
+        case 'Armor':
+         itemCategory = getRandomElement(armorTypes);
+        break;
+        case 'Accessory':
+         itemCategory = getRandomElement(accessoryTypes);
+        break;
+  default:
+    console.log(`Error`);
+}
+    switch (itemCategory) {                  
+        case 'Sword':
+         itemType = getRandomElement(swordTypes);
+        break;
+        case 'Axe':
+         itemType = getRandomElement(axeTypes);
+        break;
+        case 'Dagger':
+         itemType = getRandomElement(daggerTypes);
+        break;
+        case 'Polearm':
+         itemType = getRandomElement(polearmTypes);
+        break;
+        case 'Staff':
+         itemType = getRandomElement(staffTypes);
+        break;        
+        case 'Head':
+         itemType = getRandomElement(head_armor);
+        break;
+        case 'Shoulder':
+         itemType = getRandomElement(shoulder_armor);
+        break;
+        case 'Chest':
+         itemType = getRandomElement(chest_armor);
+        break;
+        case 'Waist':
+         itemType = getRandomElement(waist_armor);
+        break;
+        case 'Wrist':
+         itemType = getRandomElement(wrist_armor);
+        break; 
+        case 'Hands':
+         itemType = getRandomElement(hands_armor);
+        break;
+        case 'Feet':
+         itemType = getRandomElement(feet_armor);
+        break;
+        case 'Back':
+         itemType = getRandomElement(back_armor);
+        break;
+        case 'Ring'
+         itemType = getRandomElement(ring_types);
+        break;
+        case 'Neck'
+         itemType = getRandomElement(necklace_types);
+        break; 
+        case 'Trinket'
+         itemType = getRandomElement(trinket_types);
+        break;         
+  default:
+    console.log(`Error`);
+}
+return itemType;
+
+
+
+function getBelievableItemName(pItemType) {
+    itemType = pItemType;
+    class ItemAdjective {
+    constructor(pWord, pValue) {
+        this.word = pWord;
+        this.value = pValue;
+    }
+}
+function getItemShape(){
+    class ItemShape {
+    constructor(pType, pShape) {
+        this.type = pType;
+        this.shape = pShape;
+    }
+}
+}
+
     const owners = ["Jeff", "Andrew", "Stik", "Joe", "Catherine", "Egg", "Plant"];
+    const 
     const adjectives = [
         new ItemAdjective("Great", 3.0),
         new ItemAdjective("Crappy", -2.0),
         new ItemAdjective("Small", 0.9),
         new ItemAdjective("Blue", 83)
     ];
-    const things = ["Weapon", "Armor", "Accessory"];
     const owner = getRandomElement(owners);
-    const adjective = getRandomElement(adjectives);
-    const thing = getRandomElement(things);
-	const weaponTypes = ["Sword","Axe","Dagger","Polearm","Staff"];
-	const armorTypes = ["Head","Shoulder","Chest","Waist","Wrist","Hands","Feet","Back"];
-	const accessoryTypes = ["Neck","Ring","Trinket"];
-	const itemType = "";
-	const swordTypes = ["Claymore",
-						"Rapier",
-						"Katana",
-						"Cutlass",
-						"Longsword",
-						"Scimitar",
-						"Sabre",
-						"Bastard sword",
-						"Epee",
-						"Gladius"]
-						
-	const axeTypes	= ["Wood Axe",
-						"Tomahawk",
-						"Labrys",
-						"Bearded axe",
-						"Throwing axe",
-						"Bardiche",
-						"Double Axe",
-						"Cleaver",
-						"Broad Axe"
-						"Battle-axe"]
-						
-	const daggerTypes = ["Kris",
-						"Dirk",
-						"Main-gauche",
-						"Stiletto",
-						"Rondel",
-						"Baselard"]
-						
-	const polearmTypes = ["Glaive",
-						"Halberd",
-						"Pike",
-						"Partisan",
-						"Naginata",
-						"Voulge",
-						"Bec de corbin",
-						"War scythe",
-						"Ranseur"]
-						
-	const staffTypes = ["Bo staff",
-						"Quarterstaff",
-						"Jo staff",
-						"Shillelagh",
-						"Yari",
-						"Gunbai",
-						"Sansetsukon"]
-	const selectedSwordType = getRandomElement(swordTypes);
-	const selectedAxeType = getRandomElement(axeTypes);
-	const selectedDaggerType = getRandomElement(daggerTypes);
-	const selectedPolearmType = getRandomElement(polearmTypes);
-	const selectedStaffType = getRandomElement(staffTypes);
+    const adjective = getRandomElement(adjectives);				
 	
-	switch (thing) {
-		case 'Weapon':
-			let itemType = getRandomElement(weaponTypes)
-		break;
-		case 'Armor':
-			let itemType = getRandomElement(armorTypes)
-		break;
-		case 'Accessory':
-			let itemType = getRandomElement(accessoryTypes)
-		break;
-  default:
-    console.log(`Error`);
-}
-
-	switch (itemType) {
-		case 'Sword':
-			let itemType = getRandomElement(selectedSwordType)
-		break;
-		case 'Axe':
-			let itemType = getRandomElement(selectedAxeType)
-		break;
-		case 'Dagger':
-			let itemType = getRandomElement(selectedDaggerType)
-		break;
-		case 'Polearm':
-			let itemType = getRandomElement(selectedPolearmType)
-		break;
-		case 'Staff':
-			let itemType = getRandomElement(selectedStaffType)
-		break;
-		
-  default:
-    console.log(`Error`);
-}
-						
-	
-    // Calculate gridSizeX and gridSizeY based on the item type
+  /*  // Calculate gridSizeX and gridSizeY based on the item type
     if (thing === "Weapon" || thing === "Armor") {
         gridSizeX = 2;
         gridSizeY = getRandomInt(2, 4); // Random number between 2 and 4 for gridSizeY
     } else {
         gridSizeX = 1;
         gridSizeY = 1;
-    }
+    }*/
 
-    return [owner + "'s " + adjective.word + " " + thing, adjective.value, thing, gridSizeX, gridSizeY];
+    return [owner + "'s " + adjective.word + " ", getItemType(), adjective.value, 2, 4];
 }
